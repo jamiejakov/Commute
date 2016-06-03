37641594158e8d3daaad0e3f4b8fac7626df2afe
Vadims Brodskis <jamiejakov@me.com>
2016-06-03 22:48:51 +1000

    - Alerts Activity before it gets deleted :(

--- app/src/main/java/com/id11303765/commute/view/AlertsFragment.java
+++ app/src/main/java/com/id11303765/commute/view/AlertsFragment.java
@@ -1,13 +1,20 @@
 package com.id11303765.commute.view;
 
 import android.app.Fragment;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.id11303765.commute.R;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 public class AlertsFragment extends Fragment {
@@ -33,10 +40,76 @@ public class AlertsFragment extends Fragment {
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         getActivity().setTitle(R.string.alerts);
+        GetAlertsAsync getAlerts = new GetAlertsAsync();
+        getAlerts.execute();
     }
 
     @Override
     public void onStop(){
         super.onStop();
+    }
+
+    private void getAlerts(){
+        StringBuilder urlBuilder = new StringBuilder("https://api.transport.nsw.gov.au/v1/gtfs/alerts/sydneytrains");
+        URL url = null;
+        try {
+            url = new URL(urlBuilder.toString());
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Authorization", "Bearer 5e290175-9398-439d-8159-29a3bd141d1e");
+            System.out.println("Response code: " + conn.getResponseCode());
+            BufferedReader rd;
+            if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+                rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            } else {
+                rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+            }
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = rd.readLine()) != null) {
+                sb.append(line+"\n");
+
+            }
+            rd.close();
+
+            conn.disconnect();
+            System.out.println(sb.toString());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Loads the list of Timetables connecting the 2 stops for the daily commute.
+     */
+    private class GetAlertsAsync extends AsyncTask<Void, Void, Void> {
+
+        private ProgressDialog mDialog;
+
+
+        GetAlertsAsync() {
+        }
+
+        @Override
+        protected void onPreExecute() {
+            mDialog = new ProgressDialog(getActivity());
+           // mDialog.show();
+            mDialog.setMessage(getString(R.string.loading_alerts));
+            mDialog.setCancelable(false);
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            getAlerts();
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            if (this.mDialog.isShowing()) {
+                this.mDialog.dismiss();
+            }
+        }
     }
 }
